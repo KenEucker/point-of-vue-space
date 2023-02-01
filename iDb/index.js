e5a18da2df8528c205f8d3ff54ad1fdbeabb604a
Ken Eucker <keneucker@gmail.com>
2023-02-01 01:11:36 -0800

    Parses the layout.

--- iDb/index.js
+++ iDb/index.js
@@ -1,10 +1,10 @@
-const app = require('./app')
+const layouts = require('./layouts')
 const nuxt = require('./nuxt')
 const pages = require('./pages')
 const type = require('./type')
 
 module.exports = {
-  app,
+  layouts,
   nuxt,
   pages,
   type,
--- iDb/layouts.js
+++ iDb/layouts.js
@@ -1,3 +1,100 @@
 module.exports = {
-  default: {},
+  navLinks: [
+    {
+      label: 'Mods',
+      href: '/mods',
+    },
+    {
+      label: 'Plugins',
+      href: '/plugins',
+    },
+    {
+      label: 'Data Packs',
+      href: '/datapacks',
+    },
+    {
+      label: 'Shaders',
+      href: '/shaders',
+    },
+    {
+      label: 'Resource Packs',
+      href: '/resourcepacks',
+    },
+    {
+      label: 'Modpacks',
+      href: '/modpacks',
+    },
+  ],
+  footerBranding: {
+    description1: 'Modrinth is',
+    split1: 'open source',
+    split1Link: 'https://github.com/modrinth',
+    description2: '.',
+    ownerLinkBase: 'https://github.com/',
+    copyright: '© Rinth, Inc.',
+  },
+  footerLinks: {
+    links1Heading: 'Privacy',
+    links1Aria: 'Legal',
+    links1: [
+      {
+        href: '/legal/terms',
+        label: 'Terms',
+      },
+      {
+        href: '/legal/privacy',
+        label: 'Privacy',
+      },
+      {
+        href: '/legal/rules',
+        label: 'Rules',
+      },
+      {
+        href: 'https://careers.modrinth.com',
+        label: 'Careers',
+        bubble: '2',
+        external: true,
+      },
+    ],
+    links2Heading: 'Resources',
+    links2Aria: 'Resources',
+    links2: [
+      {
+        href: 'https://blog.modrinth.com',
+        label: 'Blog',
+      },
+      {
+        href: 'https://docs.modrinth.com',
+        label: 'Docs',
+      },
+      {
+        href: 'https://status.modrinth.com',
+        label: 'Status',
+      },
+      {
+        href: 'https://github.com/modrinth',
+        label: 'GitHub',
+      },
+    ],
+    links3Heading: 'Interact',
+    links3Aria: 'Interact',
+    links3: [
+      {
+        href: 'https://discord.gg/EUHuJHt',
+        label: 'Discord',
+      },
+      {
+        href: 'https://twitter.com/modrinth',
+        label: 'Twitter',
+      },
+      {
+        href: 'https://floss.social/@modrinth',
+        label: 'Mastodon',
+      },
+      {
+        href: 'https://crowdin.com/project/modrinth',
+        label: 'Crowdin',
+      },
+    ],
+  },
 }
