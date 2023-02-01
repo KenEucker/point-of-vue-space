1f1e9c01a33789016a11418a846e40ba2aa439c1
Ken Eucker <keneucker@gmail.com>
2023-01-31 17:39:20 -0800

    Finishes parsing all filters for the search.

---
+++ pages/search/EnvironmentsFilter.vue
@@ -0,0 +1,54 @@
+<template>
+  <section
+    v-if="
+      !['resourcepack', 'plugin', 'shader', 'datapack'].includes(projectType)
+    "
+    aria-label="Environment filters"
+  >
+    <h3 class="sidebar-menu-heading">Environments</h3>
+    <SearchFilter
+      :active-filters="selectedEnvironments"
+      display-name="Client"
+      facet-name="client"
+      @toggle="$emit('toggleEnv')"
+    >
+      <ClientIcon aria-hidden="true" />
+    </SearchFilter>
+    <SearchFilter
+      :active-filters="selectedEnvironments"
+      display-name="Server"
+      facet-name="server"
+      @toggle="$emit('toggleEnv')"
+    >
+      <ServerIcon aria-hidden="true" />
+    </SearchFilter>
+  </section>
+</template>
+
+<script>
+import SearchFilter from '~/components/ui/search/SearchFilter'
+import ClientIcon from '~/assets/images/categories/client.svg?inline'
+import ServerIcon from '~/assets/images/categories/server.svg?inline'
+
+export default {
+  name: 'EnvironmentsFilter',
+  components: {
+    ClientIcon,
+    SearchFilter,
+    ServerIcon,
+  },
+  props: {
+    selectedEnvironments: {
+      type: Array,
+      default: () => [],
+      deep: true,
+      sync: true,
+    },
+    projectType: {
+      type: String,
+      default: null,
+    },
+  },
+  emits: ['toggleEnv'],
+}
+</script>
---
+++ pages/search/VersionsFilter.vue
@@ -0,0 +1,97 @@
+<template>
+  <section aria-label="Platform filters">
+    <h3 class="sidebar-menu-heading">Minecraft versions</h3>
+    <Checkbox
+      v-model="showSnapshots"
+      label="Include snapshots"
+      description="Include snapshots"
+      style="margin-bottom: 0.5rem"
+      :border="false"
+    />
+    <Multiselect
+      v-model="selectedVersions"
+      :options="
+        showSnapshots
+          ? $tag.platformVersions.map((x) => x.version)
+          : $tag.platformVersions
+              .filter((it) => it.version_type === 'release')
+              .map((x) => x.version)
+      "
+      :multiple="true"
+      :searchable="true"
+      :show-no-results="false"
+      :close-on-select="false"
+      :clear-search-on-select="false"
+      :show-labels="false"
+      :selectable="() => selectedVersions.length <= 6"
+      placeholder="Choose versions..."
+      @input="$emit('onSearchChange', 1)"
+    ></Multiselect>
+    <h3 class="sidebar-menu-heading">Open source</h3>
+    <Checkbox
+      v-model="onlyOpenSource"
+      label="Open source only"
+      style="margin-bottom: 0.5rem"
+      :border="false"
+      @input="$emit('onSearchChange', 1)"
+    />
+  </section>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+import Checkbox from '~/components/ui/Checkbox'
+
+export default {
+  name: 'ProxiesFilter',
+  components: {
+    Multiselect,
+    Checkbox,
+  },
+  props: {
+    modelValue: Boolean,
+    snapshots: {
+      type: Boolean,
+      default: false,
+      sync: true,
+    },
+    openSource: {
+      type: Boolean,
+      default: false,
+      sync: true,
+    },
+    versions: {
+      type: Array,
+      default: () => [],
+      sync: true,
+    },
+  },
+  emits: ['onlyOpenSource', 'selectedVersions', 'showSnapshots'],
+  computed: {
+    showSnapshots: {
+      set(v) {
+        this.$emit('showSnapshots', v)
+      },
+      get() {
+        return this.snapshots
+      },
+    },
+    selectedVersions: {
+      set(v) {
+        this.$emit('selectedVersions', v)
+      },
+      get() {
+        return this.versions
+      },
+    },
+    onlyOpenSource: {
+      set(v) {
+        this.$emit('onlyOpenSource', v)
+      },
+      get() {
+        return this.openSource
+      },
+    },
+  },
+}
+</script>
